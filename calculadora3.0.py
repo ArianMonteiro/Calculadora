87d2ee9ae402189959666a1582f1d22111db779a
ArianMonteiro <156481521+ArianMonteiro@users.noreply.github.com>
2024-01-17 21:39:24 -0300

    Terceira versão.py
    Terceira versão da calculadora

--- calculadora3.0.py
+++ calculadora3.0.py
@@ -7,6 +7,7 @@ def verificaNumero(op):
         print('Selecione apenas um dos valores do Menu!')
         sleep(3)
         break
+
 def soma():
     n1 = float(input('Valor: '))
     total = 0
@@ -14,13 +15,20 @@ def soma():
         total = total + n1
         n1 = float(input('Para obter o total digite 000.\nNovo valor: '))
     return total
+
 def subtracao():
+    itens = []
     n1 = float(input('Valor: '))
-    total = 0
     while n1 != 000:
-        total = n1 - total
         n1 = float(input('Para obter o total, digite 000.\nNovo valor: '))
+        itens.append(n1)
+    else:
+        qtd = len(itens)
+        c = 0
+        for c in range(c, qtd):
+            total = itens[qtd] - itens[qtd + 1]
     return total
+
 def multiplica():
     n1 = float(input('Valor: '))
     total = 0
@@ -28,16 +36,19 @@ def multiplica():
         total = total * n1
         n1 = float(input('Para obter o total, digite 000.\nNovo valor: '))
     return total
+
 def divide():
     n1 = float(input('Primeiro valor: '))
     n2 = float(input('Segundo valor: '))
     total = n1 / n2
     return total
+
 def pot():
     n1 = int(input('Base: '))
     n2 = int(input('Expoente: '))
     total = print(f'{n1} ** {n2} = {math.pow(n1,n2)}')
     return total
+
 def fatorial():
     c = int(input('Valor a ser calculado: '))
     total = c
@@ -45,6 +56,7 @@ def fatorial():
         total = total * (c - 1)
     final = print(f'Fatorial = {total}')
     return final
+
 def media():
     n1 = float(input('Digite um valor:'))
     cont = 0
@@ -57,20 +69,32 @@ def media():
         total = soma / cont
         resultado = print(f'Média total: {total}')
     return resultado 
+
 def tabuada():
-    teste = teste * 1 #GERAR FUNÇÃO PARA ELABORAR A TABUADA
+    tbl_tabuada = []
+    n1 = int(input('Qual a tabuada que deseja? '))
+    c = 1
+    while c < 11:
+        resultado = print(f'{n1} * {c} = {n1 * c}')
+        tbl_tabuada.append(resultado)
+        c = c + 1
+    return tbl_tabuada
+    
 def raiz():
     n1 = float(input('Valor a ser calculado: '))        
     resultado = print(f'Raiz quadrada de {n1} = {math.sqrt(n1)}')
     return resultado
+
 def conversor():
     cot = float(input('Dólar hoje: '))
     dol = float(input('Total de dólares: USD $ '))
     reais = cot * dol
     valor = print('Total em reais: R$ ', reais)
     return valor
+
 op = input('Calculadora:\nSelecione uma das opções abaixo: \n[1] Operadores básicos \n[2] Fatorial \n[3] Média\n[4] Tabuada \n[5] Raiz quadrada \n[6] Conversor de moeda \n[7] Sair\nOpção: ')
 verificaNumero(op)
+
 op = int(op)
 match op:
     case 1:
@@ -78,7 +102,7 @@ match op:
         if operador == 1:
             print(f'O total dos valores somados é: {soma()}')
         elif operador == 2:
-            print(f'O total da subtração solicitada é: {subtracao()}') #AJUSTAR SUBTRAÇÃO PENDENTE
+            print(f'O total da subtração solicitada é: {subtracao()}') #FINALIZAR COMANDO DE SUBTRAÇÃO
         elif operador == 3:
             print(f'A muliplicação entre os valores solicitados é {multiplica()}')
         elif operador == 4:
@@ -93,10 +117,10 @@ match op:
     case 3:
         media()
     case 4:
-        n1 = int(input('Qual a tabuada que deseja? '))
-        c = 1
-        while c < 11:
-            print(f'{n1} * {c} = {n1 * c}')
+        tabuada()
+        c = 0
+        for c in range(0,9):
+            print(f'{tabuada[c]}')
             c = c + 1
     case 5:
         raiz()
@@ -106,4 +130,6 @@ match op:
         print('Obrigado por usar a calcluadora!')
         sleep(2)
         print('Até a próxima.')
+
+
 
